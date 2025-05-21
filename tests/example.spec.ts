d2d08fa045547b4dcfe6d52461a6054066eaa2fe
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-05-21 11:53:08 -0600

    Test Case 3 Added to tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('Test Case 3: Login User with incorrect email and password', async ({ page }) => {
+  await page.goto('https://automationexercise.com/');
+  await page.getByRole('link', { name: ' Signup / Login' }).click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('mymailhttps://automationexercise.com');
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').press('ControlOrMeta+z');
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('mymail@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('hello123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByText('Your email or password is').click();
 });
