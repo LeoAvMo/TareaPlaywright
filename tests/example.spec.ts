f8728cae74458b8e531781e52ba72ed279222009
LeoAvMo <avilamolinaleonardo@gmail.com>
2025-05-21 12:04:49 -0600

    Added fail test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+// Approved test case
 test('Test Case 3: Login User with incorrect email and password', async ({ page }) => {
   await page.goto('https://automationexercise.com/');
   await page.getByRole('link', { name: ' Signup / Login' }).click();
@@ -12,4 +13,31 @@ test('Test Case 3: Login User with incorrect email and password', async ({ page 
   await page.getByRole('textbox', { name: 'Password' }).fill('hello123');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByText('Your email or password is').click();
+});
+
+// Failed test case
+test('Test Case 6: Contact Us Form', async ({ page }) => {
+  await page.goto('https://automationexercise.com/');
+  await page.getByRole('link', { name: ' Contact us' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('Leo');
+  await page.getByRole('textbox', { name: 'Email', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Email', exact: true }).fill('hola');
+  await page.getByRole('textbox', { name: 'Email', exact: true }).press('Alt+@');
+  await page.getByRole('link', { name: ' Home' }).click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('mymailhttps://automationexercise.com');
+  await page.getByRole('textbox', { name: 'Email', exact: true }).dblclick();
+  await page.getByRole('textbox', { name: 'Email', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Email', exact: true }).fill('hola@ejemplo.com');
+  await page.getByRole('textbox', { name: 'Subject' }).click();
+  await page.getByRole('textbox', { name: 'Subject' }).fill('Sub');
+  await page.getByRole('textbox', { name: 'Your Message Here' }).click();
+  await page.getByRole('textbox', { name: 'Your Message Here' }).click();
+  await page.getByRole('textbox', { name: 'Your Message Here' }).fill('My message');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('link', { name: ' Home' }).click();
 });
